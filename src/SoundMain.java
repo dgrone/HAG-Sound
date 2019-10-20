5eafc0cd13c2f084ba7d0ea0ff5e2f873905266b
dgrone <daniel@gronis.de>
2019-10-20 18:44:43 +0200

    HAG-Sound: Dynamic ID

--- src/SoundMain.java
+++ src/SoundMain.java
@@ -1,19 +1,25 @@
 import HAGSound.Collections.SongAnalyse;
 import HAGSound.Disco.SoundEngine;
 import HAGSound.Disco.SpotifyDecoder;
+import HAGSound.Global.global;
 
 public class SoundMain {
 
 	public static void main(String[] args) {
 		// Song file ID from Spotify
-		String lID = "3f1xMO1C4ED8p2OMxQc1oL";
-		
-		SoundEngine lSE = new SoundEngine(lID);
-		SongAnalyse lAnalyse = SpotifyDecoder.DecodeSpotifySound(lID);
-		if(lAnalyse != null) {
-			lSE.setSoundAnalyse(lAnalyse);
-			if(lSE.PlaySound())
-				lSE.SeekAudioClipAndExecute();
+		if(args.length == 1) {
+			String lID = args[0];
+			
+			SoundEngine lSE = new SoundEngine(lID);
+			SongAnalyse lAnalyse = SpotifyDecoder.DecodeSpotifySound(lID);
+			if(lAnalyse != null) {
+				lSE.setSoundAnalyse(lAnalyse);
+				if(lSE.PlaySound())
+					lSE.SeekAudioClipAndExecute();
+			}		
+		} else {
+			global.WriteOutput("Invalid argument\r\n" + 
+					"Example: HAG-Sound.jar 3f1xMO1C4ED8p2OMxQc1oL");
 		}
 	}
 }
